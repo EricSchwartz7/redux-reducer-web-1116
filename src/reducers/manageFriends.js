cc59a7284ee96529fbfd6485710aad48541cecca
github username <github email address>
2017-02-07 11:48:08 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
 export function manageFriends(state, action){
+  if (action.type === "ADD_FRIEND"){
+    state.friends.push(action.payload)
+    return state
+  } else if (action.type === "REMOVE_FRIEND"){
+    let newFriends = []
+    state.friends.forEach (function(friend){
+      if (friend.id !== action.payload){
+        newFriends.push(friend)
+      }
+    })
+    Object.assign(state, {friends: newFriends})
+    return state
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,8 @@
 export function managePresents(state, action){
+  if (action.type === "INCREASE") {
+    let funky = state.numberOfPresents + 1
+    return {numberOfPresents: funky}
+  } else {
+    return state
+  }
 }
